c766cf3c32f764c3daf2dcc2580b1020a19bed4b
Brian Cain <bcain@quicinc.com>
2024-10-09 15:23:53 -0700

    board/qcom/sa8775: Add support for qcom sa8775-cdsp0
    Signed-off-by: Brian Cain <bcain@quicinc.com>

---
+++ board/qcom/sa8775/post_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TARGETDIR=$1
+
+cat <<EOF >> ${TARGETDIR}/etc/fstab
+devpts   /dev/pts              devpts  gid=5,mode=620  0 0
+/dev/vda /mnt/persist ext2 defaults 0 0
+debugfs  /sys/kernel/debug      debugfs  defaults  0 2
+EOF
+
