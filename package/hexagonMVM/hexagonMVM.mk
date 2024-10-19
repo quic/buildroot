630795fe54627e56acbe12d796dd26ae96b5366d
Brian Cain <brian.cain@gmail.com>
2024-10-18 21:42:53 -0500

    package/hexagonMVM: Add Hexagon MiniVM package
    Signed-off-by: Brian Cain <brian.cain@gmail.com>

---
+++ package/hexagonMVM/hexagonMVM.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# Hexagon MiniVM
+#
+################################################################################
+
+HEXAGONMVM_VERSION = 0.1.1
+HEXAGONMVM_SOURCE = $(HEXAGONMVM_VERSION).tar.gz
+HEXAGONMVM_SITE = $(call github,quic,hexagonMVM,v$(HEXAGONMVM_VERSION))
+
+HEXAGONMVM_LICENSE = BSD-3-Clause
+HEXAGONMVM_LICENSE_FILES = LICENSE
+
+HEXAGONMVM_DEPENDENCIES =
+
+define HEXAGONMVM_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC=hexagon-unknown-none-elf-clang AS=hexagon-unknown-none-elf-clang -C $(@D)
+endef
+
+define HEXAGONMVM_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) DESTDIR=$(TARGET_DIR) -C $(@D) install
+endef
+
+$(eval $(generic-package))
