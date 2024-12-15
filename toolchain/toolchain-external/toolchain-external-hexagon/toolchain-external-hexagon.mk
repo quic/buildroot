f8922fec23bfd169791e7dccfec2b81fe1083b2d
Brian Cain <brian.cain@oss.qualcomm.com>
2024-12-15 13:30:35 -0800

    fixup! toolchain/toolchain-external: add hexagon toolchain support

--- toolchain/toolchain-external/toolchain-external-hexagon/toolchain-external-hexagon.mk
+++ toolchain/toolchain-external/toolchain-external-hexagon/toolchain-external-hexagon.mk
@@ -4,11 +4,8 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_HEXAGON_VERSION = 19.1.2
+TOOLCHAIN_EXTERNAL_HEXAGON_VERSION = 19.1.5
 TOOLCHAIN_EXTERNAL_HEXAGON_SITE= https://artifacts.codelinaro.org/artifactory/codelinaro-toolchain-for-hexagon/$(TOOLCHAIN_EXTERNAL_HEXAGON_VERSION)
-TOOLCHAIN_EXTERNAL_HEXAGON_SOURCE = clang+llvm-$(TOOLCHAIN_EXTERNAL_HEXAGON_VERSION)-cross-$(TOOLCHAIN_EXTERNAL_PREFIX).tar.xz
-
-# No trampoline support in lld yet, so for now:
-TOOLCHAIN_EXTERNAL_HEXAGON_CFLAGS += -mlong-calls
+TOOLCHAIN_EXTERNAL_HEXAGON_SOURCE = clang+llvm-$(TOOLCHAIN_EXTERNAL_HEXAGON_VERSION)-cross-$(TOOLCHAIN_EXTERNAL_PREFIX).tar.zst
 
 $(eval $(toolchain-external-package))
