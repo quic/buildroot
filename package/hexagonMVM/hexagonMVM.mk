608fa8246ef96c66b88eeb3de05512484cbc0e19
Brian Cain <brian.cain@oss.qualcomm.com>
2024-12-15 13:27:35 -0800

    fixup! package/hexagonMVM: new package

--- package/hexagonMVM/hexagonMVM.mk
+++ package/hexagonMVM/hexagonMVM.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HEXAGONMVM_VERSION = 0.1.4
+HEXAGONMVM_VERSION = 0.2.0
 HEXAGONMVM_SOURCE = $(HEXAGONMVM_VERSION).tar.gz
 HEXAGONMVM_SITE = $(call github,quic,hexagonMVM,v$(HEXAGONMVM_VERSION))
 
