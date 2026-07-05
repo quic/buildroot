4d5417c945fbb202f941e16edd43bfde839b92e7
Brian Cain <brian.cain@oss.qualcomm.com>
2026-07-04 20:07:00 -0500

    toolchain: bump hexagon external toolchain to clang+llvm 22.1.4
    Updates the hexagon QEMU defconfig's custom toolchain URL/version
    (from 19.1.5) and the predefined toolchain-external-hexagon package's
    default version (from 20.1.4) to the 22.1.4 codelinaro release.
    
    The 22.1.4 release's artifactory directory has a trailing underscore
    ("22.1.4_/") unlike the tarball filename itself, so the toolchain
    package's SITE URL is adjusted accordingly.
    
    This toolchain reports clang 21.x and ships Linux 6.13 headers, so the
    defconfig's BR2_TOOLCHAIN_EXTERNAL_CLANG_* and
    BR2_TOOLCHAIN_EXTERNAL_HEADERS_* choices are bumped to the closest/
    latest available options (21.0 and "6.12.x or later" respectively);
    buildroot does not yet have explicit 22.0/6.13 choices.
    
    Signed-off-by: Brian Cain <brian.cain@gmail.com>

--- toolchain/toolchain-external/toolchain-external-hexagon/toolchain-external-hexagon.mk
+++ toolchain/toolchain-external/toolchain-external-hexagon/toolchain-external-hexagon.mk
@@ -4,8 +4,10 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_HEXAGON_VERSION = 20.1.4
-TOOLCHAIN_EXTERNAL_HEXAGON_SITE= https://artifacts.codelinaro.org/artifactory/codelinaro-toolchain-for-hexagon/$(TOOLCHAIN_EXTERNAL_HEXAGON_VERSION)
+TOOLCHAIN_EXTERNAL_HEXAGON_VERSION = 22.1.4
+# The codelinaro artifactory release directory for this version has a
+# trailing underscore, unlike the version number embedded in the tarball name
+TOOLCHAIN_EXTERNAL_HEXAGON_SITE= https://artifacts.codelinaro.org/artifactory/codelinaro-toolchain-for-hexagon/$(TOOLCHAIN_EXTERNAL_HEXAGON_VERSION)_
 TOOLCHAIN_EXTERNAL_HEXAGON_SOURCE = clang+llvm-$(TOOLCHAIN_EXTERNAL_HEXAGON_VERSION)-cross-$(TOOLCHAIN_EXTERNAL_PREFIX).tar.zst
 
 $(eval $(toolchain-external-package))
